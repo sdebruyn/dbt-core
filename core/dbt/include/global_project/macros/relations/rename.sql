9b54a15313602ed175e125b1bfa140c953edc130
Sam Debruyn <sam@debruyn.dev>
2023-11-20 23:44:11 +0100

    remove whitespace to make tests succeed

--- core/dbt/include/global_project/macros/relations/rename.sql
+++ core/dbt/include/global_project/macros/relations/rename.sql
@@ -30,6 +30,6 @@
 {% macro default__rename_relation(from_relation, to_relation) -%}
   {% set target_name = adapter.quote_as_configured(to_relation.identifier, 'identifier') %}
   {% call statement('rename_relation') -%}
-    {{ get_rename_sql(from_relation, target_name) }}
+    {{ get_rename_sql(from_relation, target_name) -}}
   {%- endcall %}
 {% endmacro %}
